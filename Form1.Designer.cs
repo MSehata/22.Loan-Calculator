7522a8d822496212a62bea4f1a5cd89079e2b837
Moloko Sehata <moloko4@live.com>
2023-10-12 11:41:54 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace _22.Loan_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoanAmount = new System.Windows.Forms.Label();
+            this.txtLoanAmount = new System.Windows.Forms.TextBox();
+            this.DownPayment = new System.Windows.Forms.Label();
+            this.txtDownPayment = new System.Windows.Forms.TextBox();
+            this.IR = new System.Windows.Forms.Label();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.Term = new System.Windows.Forms.Label();
+            this.txtTerm = new System.Windows.Forms.TextBox();
+            this.btnMortgage = new System.Windows.Forms.Button();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblMonthlyPayment = new System.Windows.Forms.Label();
+            this.lblMonthlyPaymentAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoanAmount
+            // 
+            this.LoanAmount.AutoSize = true;
+            this.LoanAmount.Location = new System.Drawing.Point(94, 73);
+            this.LoanAmount.Name = "LoanAmount";
+            this.LoanAmount.Size = new System.Drawing.Size(85, 16);
+            this.LoanAmount.TabIndex = 0;
+            this.LoanAmount.Text = "Loan Amount";
+            // 
+            // txtLoanAmount
+            // 
+            this.txtLoanAmount.Location = new System.Drawing.Point(254, 70);
+            this.txtLoanAmount.Name = "txtLoanAmount";
+            this.txtLoanAmount.Size = new System.Drawing.Size(143, 22);
+            this.txtLoanAmount.TabIndex = 1;
+            // 
+            // DownPayment
+            // 
+            this.DownPayment.AutoSize = true;
+            this.DownPayment.Location = new System.Drawing.Point(94, 127);
+            this.DownPayment.Name = "DownPayment";
+            this.DownPayment.Size = new System.Drawing.Size(97, 16);
+            this.DownPayment.TabIndex = 0;
+            this.DownPayment.Text = "Down Payment";
+            // 
+            // txtDownPayment
+            // 
+            this.txtDownPayment.Location = new System.Drawing.Point(254, 124);
+            this.txtDownPayment.Name = "txtDownPayment";
+            this.txtDownPayment.Size = new System.Drawing.Size(143, 22);
+            this.txtDownPayment.TabIndex = 1;
+            // 
+            // IR
+            // 
+            this.IR.AutoSize = true;
+            this.IR.Location = new System.Drawing.Point(94, 187);
+            this.IR.Name = "IR";
+            this.IR.Size = new System.Drawing.Size(82, 16);
+            this.IR.TabIndex = 0;
+            this.IR.Text = "Interest Rate";
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(254, 184);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(143, 22);
+            this.txtInterestRate.TabIndex = 1;
+            // 
+            // Term
+            // 
+            this.Term.AutoSize = true;
+            this.Term.Location = new System.Drawing.Point(94, 249);
+            this.Term.Name = "Term";
+            this.Term.Size = new System.Drawing.Size(91, 16);
+            this.Term.TabIndex = 0;
+            this.Term.Text = "Term in Years";
+            // 
+            // txtTerm
+            // 
+            this.txtTerm.Location = new System.Drawing.Point(254, 246);
+            this.txtTerm.Name = "txtTerm";
+            this.txtTerm.Size = new System.Drawing.Size(143, 22);
+            this.txtTerm.TabIndex = 1;
+            // 
+            // btnMortgage
+            // 
+            this.btnMortgage.Location = new System.Drawing.Point(151, 313);
+            this.btnMortgage.Name = "btnMortgage";
+            this.btnMortgage.Size = new System.Drawing.Size(93, 29);
+            this.btnMortgage.TabIndex = 2;
+            this.btnMortgage.Text = "Mortgage";
+            this.btnMortgage.UseVisualStyleBackColor = true;
+            this.btnMortgage.Click += new System.EventHandler(this.btnMortgage_Click);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(265, 313);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(78, 29);
+            this.btnAuto.TabIndex = 2;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(210, 392);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblMonthlyPayment
+            // 
+            this.lblMonthlyPayment.AutoSize = true;
+            this.lblMonthlyPayment.Location = new System.Drawing.Point(94, 355);
+            this.lblMonthlyPayment.Name = "lblMonthlyPayment";
+            this.lblMonthlyPayment.Size = new System.Drawing.Size(122, 16);
+            this.lblMonthlyPayment.TabIndex = 0;
+            this.lblMonthlyPayment.Text = "Monthly Payment: $";
+            // 
+            // lblMonthlyPaymentAmount
+            // 
+            this.lblMonthlyPaymentAmount.AutoSize = true;
+            this.lblMonthlyPaymentAmount.Location = new System.Drawing.Point(222, 355);
+            this.lblMonthlyPaymentAmount.Name = "lblMonthlyPaymentAmount";
+            this.lblMonthlyPaymentAmount.Size = new System.Drawing.Size(22, 16);
+            this.lblMonthlyPaymentAmount.TabIndex = 0;
+            this.lblMonthlyPaymentAmount.Text = "     ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMortgage);
+            this.Controls.Add(this.txtTerm);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtDownPayment);
+            this.Controls.Add(this.txtLoanAmount);
+            this.Controls.Add(this.lblMonthlyPaymentAmount);
+            this.Controls.Add(this.lblMonthlyPayment);
+            this.Controls.Add(this.Term);
+            this.Controls.Add(this.IR);
+            this.Controls.Add(this.DownPayment);
+            this.Controls.Add(this.LoanAmount);
+            this.Name = "Form1";
+            this.Text = "Loan Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LoanAmount;
+        private System.Windows.Forms.TextBox txtLoanAmount;
+        private System.Windows.Forms.Label DownPayment;
+        private System.Windows.Forms.TextBox txtDownPayment;
+        private System.Windows.Forms.Label IR;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.Label Term;
+        private System.Windows.Forms.TextBox txtTerm;
+        private System.Windows.Forms.Button btnMortgage;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblMonthlyPayment;
+        private System.Windows.Forms.Label lblMonthlyPaymentAmount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22.Loan_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToParent();
+            Opacity = 0.95;
+        }
+
+        private void btnMortgage_Click(object sender, EventArgs e)
+        {
+            double loanAmount = 0.0;
+            double downPayment = 0.0;
+            double interestRate = 0.0;
+            double monthlyPayment = 0.0;
+            int term = 0;
+
+            if (!String.IsNullOrEmpty(txtLoanAmount.Text))
+                loanAmount = Convert.ToDouble(txtLoanAmount.Text);
+
+            if (!String.IsNullOrEmpty(txtDownPayment.Text))
+                downPayment = Convert.ToDouble(txtDownPayment.Text);
+
+            if (!String.IsNullOrEmpty(txtInterestRate.Text))
+                interestRate = Convert.ToDouble(txtInterestRate.Text);
+
+            if (!String.IsNullOrEmpty(txtTerm.Text))
+                term = Convert.ToInt32(txtTerm.Text);
+
+            int termsinMonths = term * 12;
+
+            monthlyPayment = (loanAmount - downPayment) * (Math.Pow((1 + interestRate / 12),termsinMonths)
+                * interestRate) / (12 * (Math.Pow((1 + interestRate / 12), termsinMonths) - 1));
+
+            monthlyPayment = Math.Round(monthlyPayment, 2);
+
+            lblMonthlyPaymentAmount.Text = String.Format(monthlyPayment.ToString());
+
+
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            double loanAmount = 0.0;
+            double downPayment = 0.0;
+            double interestRate = 0.0;
+            double monthlyPayment = 0.0;
+            int term = 0;
+
+            if (!String.IsNullOrEmpty(txtLoanAmount.Text))
+                loanAmount = Convert.ToDouble(txtLoanAmount.Text);
+
+            if (!String.IsNullOrEmpty(txtDownPayment.Text))
+                downPayment = Convert.ToDouble(txtDownPayment.Text);
+
+            if (!String.IsNullOrEmpty(txtInterestRate.Text))
+                interestRate = Convert.ToDouble(txtInterestRate.Text);
+            
+            if (!String.IsNullOrEmpty(txtTerm.Text))
+                term = Convert.ToInt32(txtTerm.Text);
+
+            int termsinMonths = term * 12; interestRate /= 12;
+            monthlyPayment = (interestRate * loanAmount) / (1 - Math.Pow(1 + interestRate, termsinMonths *
+           -1));
+
+            monthlyPayment = Math.Round(monthlyPayment, 2);
+
+            lblMonthlyPaymentAmount.Text = String.Format(monthlyPayment.ToString());
+        }
+    }
+}
